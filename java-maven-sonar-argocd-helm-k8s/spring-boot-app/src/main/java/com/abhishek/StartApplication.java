2a0e56b97b06344245dd41a1e436389a81aadda8
Laharis1220 <81207186+Laharis1220@users.noreply.github.com>
2023-11-08 17:46:01 +0530

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Hello Lahari! Congratulations on deploying your first Jenkins CI-CD pipeline");
-        model.addAttribute("msg", "Special thanks to Santosh for helping you thru out the application. Fine");
+        model.addAttribute("msg", "Special thanks to Santosh for helping you thru out the application. Fine!");
         return "index";
     }
 
